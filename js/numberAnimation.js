75add915a957399baeeb40a0eec0e1161e36d349
igorchagov <igorchagov@mail.ru>
2023-05-18 11:51:05 +0300

    some change

---
+++ js/numberAnimation.js
@@ -0,0 +1,43 @@
+let numbers1 = document.querySelectorAll('.count-number--1');
+let numbers2 = document.querySelectorAll('.count-number--2');
+let numberBlock = document.querySelector('.details__inner');
+let expBlock = document.querySelector('.exp__container');
+
+for (let i = 0; i < numbers1.length; i++) {
+  $(document).ready(function () {
+    var show = true;
+    $(window).on("scroll load resize", function () {
+      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+      let b_height = $(numberBlock).outerHeight() // высота общего блока
+      let w_height = $(window).height(); // Высота окна браузера
+      if (numbers1[i].getBoundingClientRect().top <= ((w_height / 1.2) + (b_height / 2))) {
+        $(numbers1[i]).css('opacity', '1');
+        $(numbers1[i]).spincrement({
+          thousandSeparator: ' ',
+          duration: 2000
+        });
+        show = false;
+      }
+    });
+  });
+}
+// // //
+// // //
+for (let i = 0; i < numbers2.length; i++) {
+  $(document).ready(function () {
+    var show = true;
+    $(window).on("scroll load resize", function () {
+      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+      let b_height = $(expBlock).outerHeight() // высота общего блока
+      let w_height = $(window).height(); // Высота окна браузера
+      if (numbers2[i].getBoundingClientRect().top <= ((w_height / 1.2) + (b_height / 2))) {
+        $(numbers2[i]).css('opacity', '1');
+        $(numbers2[i]).spincrement({
+          thousandSeparator: ' ',
+          duration: 2000
+        });
+        show = false;
+      }
+    });
+  });
+}
