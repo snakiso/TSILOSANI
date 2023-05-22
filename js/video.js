bf3c9071ab2ea48e3b2e8230838037fbc9d6ba10
igorchagov <igorchagov@mail.ru>
2023-05-22 14:22:38 +0300

    fixed

--- js/video.js
+++ js/video.js
@@ -2,3 +2,9 @@
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
 });
+
+let video = document.querySelector('.hero__video');
+
+video.addEventListener("loadeddata", () => {
+  video.classList.remove('hero__video--hidden')
+})
